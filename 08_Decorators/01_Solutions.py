90ca841d107fdac67db5d8530f7fabe8f12fd1bd
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-18 19:28:31 +0530

    Decorators

---
+++ 08_Decorators/01_Solutions.py
@@ -0,0 +1,25 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        
+        print(f"args: {args} kwargs: {kwargs}")
+
+        start = time.time()
+
+        result = func(*args, **kwargs)
+
+        end = time.time()
+
+        print(f'{func.__name__} ran in {end - start} time')
+
+
+    return wrapper
+
+
+@timer
+def example_func(n):
+    time.sleep(n)
+
+
+example_func(2)
---
+++ 08_Decorators/03_solutions.py
@@ -0,0 +1,27 @@
+import time
+
+def cache(func):
+    cache_value = {}
+    print(cache_value)
+    def wrapper(*args, **kwargs):
+        
+        if args in cache_value:
+            return cache_value[args]
+        
+        
+        result = func(*args,**kwargs) 
+        cache_value[args] = result
+        return result
+
+    return wrapper
+
+
+@cache
+def long_running(a,b):
+    time.sleep(4)
+    return a + b
+
+
+print(long_running(2,3))
+print(long_running(2,3))
+print(long_running(2,8))
