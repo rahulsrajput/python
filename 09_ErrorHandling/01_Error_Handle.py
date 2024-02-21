3de9584aec3af922478460fd278f59bccdf94c13
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-21 15:02:23 +0530

    ErrorHandling

---
+++ 09_ErrorHandling/01_Error_Handle.py
@@ -0,0 +1,12 @@
+# file = open('youtube.txt','w')
+
+# try:
+#     file.write('chai or code')
+# finally:
+#     file.close()
+
+
+
+# 2 easy way...
+with open('youtube.txt','w') as file:
+    file.write('chaiii or waterr..')
